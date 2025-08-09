c2d58b59785e6a107e5ff03f098b8386ac6c4ac0
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-09 22:56:38 +0600

    Update: Fies updated

--- circ.cpp
+++ circ.cpp
@@ -1,7 +1,20 @@
-#include <istream>
+#include <bits/stdc++.h>
+using namespace std;
+
 int main() {
-    int r;
-    cin >> r;
-    cout << r;
+    set<string> key;
+    string input;
+    
+    for(int i = 1; i <= 4; i++) {
+        cin >> input;          
+        key.insert(s);     
+    }
+
+    if (key.size() == 4) {   
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+
     return 0;
 }
---
+++ rest_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+// VEXTOR PRACTICING 
+int main() {
+    vector <int> myVec;
+    return 0;
+}
