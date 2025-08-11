4e8cd8edaaaee7dddbdafe33b75ce5f1063070a8
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-11 23:09:52 +0600

    Update: Another Cod

---
+++ TEST/another.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    while(1) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            cout << "vai ter copa!\n";
+        } else if (n >= 1 && n <= 100) {
+            cout << "vai ter duas!\n";
+        }
+    }
+    return 0;
+}
