a30b6d12110015a3b7fe24f4c74c331671e4de41
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-21 17:54:21 +0600

    UpdateL Filesl

---
+++ BW/one.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b; 
+    int c = a + b;
+    string nana = "Emran Ali";
+    string nani = "Bushrah Emran";
+    // biyaa
+    cout << nana <<  << endl;
+    return 0;
+}
