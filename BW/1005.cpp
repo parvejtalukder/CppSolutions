4ea494c6793c7a42fc01ecb9fc41fa04acb8ee61
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-31 14:13:02 +0600

    Update: New chnages

--- BW/1005.cpp
+++ BW/1005.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    ios_base::sync_with_stdio;
-    cin.tie(0);
-    double one, two;
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+    float one, two;
     cin >> one >> two;
-    cout << fixed << setprecision(5) << "MEDIA = " << (one + two) / 2.00 << endl;
+    float average = ((one * 3.5) + (7.5 * two)) / 3.5 + 7.5;
+    cout << fixed << setprecision(5);
+    cout << "MEDIA = " << average << endl;
     return 0;
 }
