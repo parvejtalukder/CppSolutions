c1adee0da240dfaa94a8634e54add4121131a96d
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-04 00:12:53 +0600

    Update: New change on list

---
+++ STL/List/one.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    list <char> name;
+    name.push_back('P');    
+    name.push_back('a');   
+    name.push_back('r');
+    name.push_back('v');    
+    name.push_back('e');    
+    name.push_back('j');
+    list <char>::iterator printer;
+    for(printer = name.begin(); printer != name.end(); printer++) {
+        cout << *printer;
+    }
+    cout << endl;
+    return 0;
+}
