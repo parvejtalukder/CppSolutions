1dee6bc80710e0a05abfdaaec9413711f5183974
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-04 00:17:00 +0600

    Update: New Changes

--- STL/List/one.cpp
+++ STL/List/one.cpp
@@ -8,9 +8,12 @@ int main() {
     name.push_back('v');    
     name.push_back('e');    
     name.push_back('j');
-    list <char>::iterator printer;
-    for(printer = name.begin(); printer != name.end(); printer++) {
-        cout << *printer;
+    // list <char>::iterator printer;
+    // for(printer = name.begin(); printer != name.end(); printer++) {
+    //     cout << *printer;
+    // }
+    for(auto it: name) {
+        cout << it;
     }
     cout << endl;
     return 0;
