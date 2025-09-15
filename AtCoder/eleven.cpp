a8c3700bd3682664088cb9d3486cb13f4c3285e8
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-15 10:37:57 +0600

    Update: New Update

---
+++ AtCoder/eleven.cpp
@@ -0,0 +1,28 @@
+
+/*https://vjudge.net/problem/codeforces-598a*/
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int nums;
+vector <long long int> num;
+
+void twpPowNum() {
+    
+}
+
+void pht() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    pht();
+    int n; cin >> n;
+    int sum = 0; 
+    for(int i = 1; i <= n; i++) {
+        
+    }
+    cout << sum << endl;
+    return 0;
+}
--- AtCoder/ten.cpp
+++ AtCoder/ten.cpp
@@ -12,8 +12,8 @@ int main() {
     pht();
     int s, t; cin >> s >> t; int cnt = 0;
     for(int a = 0; a <= s; a++) {
-        for(int b = 0; b <= a + b; b++) {
-            for (int c = 0; c <= a + b + c; c++) {
+        for(int b = 0; a + b <= s; b++) {
+            for (int c = 0; a + b + c <= s; c++) {
                 if (a * b * c <= t) cnt++;
             }
         }
