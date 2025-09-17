c349725e2435e88936b6ef52961ac998bba00d1d
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-18 00:55:34 +0600

    New changes

---
+++ LocalWorks/STL/List/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list <int> myList;
+    int sum = 5 + 8;
+    cout << sum << endl;
+    myList.push_front(sum);
+    return 0;
+}
