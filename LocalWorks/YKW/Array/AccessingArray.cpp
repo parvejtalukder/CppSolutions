86f749440fe0eb5fc45e590fac05db16d6183a7f
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-18 01:13:00 +0600

    new changes

--- LocalWorks/YKW/Array/AccessingArray.cpp
+++ LocalWorks/YKW/Array/AccessingArray.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-    int age[10];
+    int n; cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    } 
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << endl; 
+    }
     return 0;
 }
