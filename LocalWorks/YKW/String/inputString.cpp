5e399c48169363127a64aec12114e404de394112
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-18 10:28:59 +0600

    Update: New Chnages

---
+++ LocalWorks/YKW/String/inputString.cpp
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char myName[40];
+    cin.getline(myName, 40);
+    int size = strlen(myName);
+    cout << myName << endl << size << endl;
+    int charSize;
+    charSize = size;
+    return 0;
+}
