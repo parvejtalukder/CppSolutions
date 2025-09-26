e6bf6dbb913e2bf2f2ac98c4a410b1e4962049e5
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-26 11:34:43 +0600

    Update: New Changes

--- CodeForces/MWSDmqGsZm/219774/x.cpp
+++ CodeForces/MWSDmqGsZm/219774/x.cpp
@@ -9,6 +9,16 @@ void pht() {
 
 int main() {
     pht();
+    int row, col;
+    cin >> row >> col;
+    int home[row][col];
+    for(int i = 0; i < row; i++) {
+        for(int j = i; j < col; j++) {
+            cin >> home[i][j];
+        }
+    }
+    int neRow, neCol;
+    cin >> neRow >> neCol;
     
     return 0;
 }
