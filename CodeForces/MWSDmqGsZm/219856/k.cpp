cb15df9419ea9b9212980a03c9758fd62af911f2
parvejtalukder <mail@parvejhusentalukder.com>
2025-09-28 19:02:16 +0600

    Update: New Changes

--- CodeForces/MWSDmqGsZm/219856/k.cpp
+++ CodeForces/MWSDmqGsZm/219856/k.cpp
@@ -10,6 +10,22 @@ void pht() {
 
 int main() {
     pht();
-    
+    int testCase;
+    cin >> testCase;
+    while(testCase--) {
+        string One, Two;
+        cin >> One; cin >> Two;
+        int oneSize = One.size();
+        int TwoSize = Two.size();
+        for(int i = 0; i < max(oneSize, TwoSize); i++) {
+            if (i < oneSize) {
+                cout << One[i];
+            }
+            if (i < TwoSize) {
+                cout << Two[i];
+            }
+        }
+        cout << endl;
+    }
     return 0; 
 }
