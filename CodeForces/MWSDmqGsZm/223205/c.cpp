e8d9a3695b5ec0347e26dacafb2b14cbc12eaa23
parvejtalukder <mail@parvejhusentalukder.com>
2025-10-07 11:46:09 +0600

    Update: New Changes

---
+++ CodeForces/MWSDmqGsZm/223205/c.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string binNum;
+
+void pht() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+string toBin(int num) {
+    
+    return binNum;
+}
+
+int main() {
+    pht();
+    
+    return 0;
+}
