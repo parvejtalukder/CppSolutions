7b46910d617a42269396ecd86c5552e77a196131
parvejtalukder <mail@parvejhusentalukder.com>
2025-12-27 23:48:13 +0600

    Update: New Changges on AtCoder

---
+++ AtCoder/Digits.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc156/tasks/abc156_b
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pht() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+
+int main() {
+    pht();
+
+    long long n, k;
+    cin >> n >> k;
+    
+
+    return 0;
+}
